2a3c2b362bed88ff4ff279c767d6b210dedfdfa1
Alabaika <14alabaika88@gmail.com>
2021-07-04 13:12:36 +0300

    6

---
+++ src/state.js
@@ -0,0 +1,21 @@
+let postData = [
+    {message: "Hi, how are you?", likes: 15},
+    {message: "It is may be cool", likes: 23},
+    {message: "It is may be bad", likes: 47}
+]
+
+let dialogsData = [
+    {id: 1, name: "Sasha"},
+    {id: 2, name: "Krystyna"},
+    {id: 3, name: "Tina"},
+    {id: 4, name: "Syliko"},
+    {id: 5, name: "Iordan"},
+    {id: 6, name: "Iordan"}
+]
+let messagesData = [
+    {message: "how are yo man?"},
+    {message: "how are yo"},
+    {message: "how are "},
+    {message: "how are yo "},
+    {message: "how are yo ma"}
+]
